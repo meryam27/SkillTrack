e7dea09b669e92a6a3b88644b9655172de96375a
EZZHAR <abdelkarimezzhar471@gmail.com>
2026-01-13 14:30:21 +0100

    Ajout de dashboard v:1

---
+++ Backend/src/app.ts
@@ -0,0 +1,29 @@
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+
+import connectDB from './config/database';
+import dashboardRoutes from './routes/dashboard';
+
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+
+// Middleware pour lire le JSON
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+
+// Connexion à la base de données
+connectDB();
+
+app.use('/api/dashboard', dashboardRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
